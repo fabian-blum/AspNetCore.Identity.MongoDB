b51c518a6e0ab4a43b4cb7c1e26d7fcea36607c9
Fabian Blum <fabian-blum@outlook.de>
2017-10-08 18:19:19 +0200

    Bugfix

--- FB.AspNetCore.Identity.MongoDB/MongoIdentityRole.cs
+++ FB.AspNetCore.Identity.MongoDB/MongoIdentityRole.cs
@@ -28,23 +28,23 @@ namespace AspNetCore.Identity.MongoDB
         /// <summary>
         /// A random value that should change whenever a role is persisted to the store
         /// </summary>
-        public virtual string ConcurrencyStamp { get; set; } = Guid.NewGuid().ToString();
+        public override string ConcurrencyStamp { get; set; } = Guid.NewGuid().ToString();
 
         /// <summary>
         /// Gets or sets the primary key for this role.
         /// </summary>
         [BsonRepresentation(BsonType.ObjectId)]
-        public virtual string Id { get; set; }
+        public override string Id { get; set; }
 
         /// <summary>
         /// Gets or sets the name for this role.
         /// </summary>
-        public virtual string Name { get; set; }
+        public override string Name { get; set; }
 
         /// <summary>
         /// Gets or sets the normalized name for this role.
         /// </summary>
-        public virtual string NormalizedName { get; set; }
+        public override string NormalizedName { get; set; }
 
     }
 }
--- Sample/Startup.cs
+++ Sample/Startup.cs
@@ -1,10 +1,15 @@
-﻿using AspNetCore.Identity.MongoDB;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCore.Identity.MongoDB;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
+using Sample.Data;
 using Sample.Models;
 using Sample.Services;
 
@@ -12,36 +17,52 @@ namespace Sample
 {
     public class Startup
     {
-        public Startup(IHostingEnvironment env)
+        public Startup(IConfiguration configuration)
         {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-                .AddJsonFile("appsettings.Mine.json", optional: true);
-
-            if (env.IsDevelopment())
-            {
-                // For more details on using the user secret store see https://go.microsoft.com/fwlink/?LinkID=532709
-                builder.AddUserSecrets<Startup>();
-            }
-
-            builder.AddEnvironmentVariables();
-            Configuration = builder.Build();
+            Configuration = configuration;
         }
 
-        public IConfigurationRoot Configuration { get; }
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.AddDbContext<ApplicationDbContext>(options =>
+            //    options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            //services.AddIdentity<ApplicationUser, IdentityRole>()
+            //    .AddEntityFrameworkStores<ApplicationDbContext>()
+            //    .AddDefaultTokenProviders();
+
+            services.AddIdentity<ApplicationUser, MongoIdentityRole>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequiredLength = 6;
+            }).AddDefaultTokenProviders();
+
+            services
+                .Configure<MongoDBOption>(Configuration.GetSection("MongoDBOption"))
+                .AddMongoDatabase()
+                .AddMongoDbContext<ApplicationUser, MongoIdentityRole>()
+                .AddMongoStore<ApplicationUser, MongoIdentityRole>();
+
+            // Add application services.
+            services.AddTransient<IEmailSender, EmailSender>();
+
+            services.AddMvc();
+        }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseBrowserLink();
+                app.UseDatabaseErrorPage();
             }
             else
             {
@@ -52,42 +73,12 @@ namespace Sample
 
             app.UseAuthentication();
 
-            // Add external authentication middleware below. To configure them please see https://go.microsoft.com/fwlink/?LinkID=532715
-            app.UseExternalAuth(Configuration);
-
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
-        }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            // Add framework services.
-            /*services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-
-            services.AddIdentity<ApplicationUser, IdentityRole>()
-                .AddEntityFrameworkStores<ApplicationDbContext>()
-                .AddDefaultTokenProviders();*/
-
-            services.AddIdentity<ApplicationUser, MongoIdentityRole>()
-                .AddDefaultTokenProviders();
-
-            services
-                .Configure<MongoDBOption>(Configuration.GetSection("MongoDBOption"))
-                .AddMongoDatabase()
-                .AddMongoDbContext<ApplicationUser, MongoIdentityRole>()
-                .AddMongoStore<ApplicationUser, MongoIdentityRole>();
-
-            services.AddMvc();
-
-            // Add application services.
-            services.AddTransient<IEmailSender, AuthMessageSender>();
-            services.AddTransient<ISmsSender, AuthMessageSender>();
         }
     }
 }
